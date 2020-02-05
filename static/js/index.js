536c744447089380f78e9b5bee3c7e14369ecdf0
Leonardo Nobrega <leo@mac-2018.local>
2020-02-05 16:54:16 -0500

    Undo changes to lines marked with the read-only class.
    This seems to work.

--- static/js/index.js
+++ static/js/index.js
@@ -2,7 +2,7 @@
 /*
 empty pad:
 
-eadonly_experiment: rep is {"lines":{},"selStart":[0,0],"selEnd":[0,0],"selFocusAtStart":false,"alltext":"\n","alines":["|1+1"],"apool":{"numToAttrib":{"0":["italic","true"],"1":["italic",""]},"attribToNum":{"italic,true":0,"italic,":1},"nextNum":2}}
+readonly_experiment: rep is {"lines":{},"selStart":[0,0],"selEnd":[0,0],"selFocusAtStart":false,"alltext":"\n","alines":["|1+1"],"apool":{"numToAttrib":{"0":["italic","true"],"1":["italic",""]},"attribToNum":{"italic,true":0,"italic,":1},"nextNum":2}}
 
 wrote the word 'hello':
 
@@ -17,9 +17,65 @@ Contents to test the pad with:
 
 curl "localhost:9001/api/1/setHTML?apikey=babysfirstetherpadapikey&padID=test" --data-urlencode "html=<html><body><span class="rox">read-only experiment</span></body></html>"
 
+Rep from editor containing text with the read-only class:
+(See the alines attribute associates the read-only attribute
+to the first line.)
+
+readonly_experiment: rep is {"lines":{},"selStart":[0,0],"selEnd":[0,0],"selFocusAtStart":false,"alltext":"read-only experiment\nblah\nhello\n\n","alines":["*0+k|1+1","*1*2*3+4*2|1+1","*1*2*3+5*2|1+1","|1+1"],"apool":{"numToAttrib":{"0":["rox","true"],"1":["ace-line","true"],"2":["author","a.c0J0o5uiRaPFjJR4"],"3":["author-a-c0z74z0o5uiz82zaz80zz70zjz74zz82z4","true"]},"attribToNum":{"rox,true":0,"ace-line,true":1,"author,a.c0J0o5uiRaPFjJR4":2,"author-a-c0z74z0o5uiz82zaz80zz70zjz74zz82z4,true":3},"nextNum":4}}
+
 */
 
 var AttributeManager = require("ep_etherpad-lite/static/js/AttributeManager");
+var AttributePool = require("ep_etherpad-lite/static/js/AttributePool");
+var Changeset = require("ep_etherpad-lite/static/js/Changeset");
+
+var readOnlyRegions = null;
+
+function getReadOnlyRegions(rep) {
+  var attributeIndex = rep.apool.attribToNum["rox,true"];
+  var opIter;
+  var op;
+  var value;
+  var regions = [];
+  var region;
+  var currentPosition = 0;
+  //console.log("apool " + JSON.stringify(rep.apool) + " alines " + rep.alines + " attributeIndex " + attributeIndex);
+  //console.log("rep " + JSON.stringify(rep));
+  rep.alines.forEach(function (aline) {
+    opIter = Changeset.opIterator(aline);
+    while (opIter.hasNext()){
+      op = opIter.next();
+      value = Changeset.opAttributeValue(op, "rox", rep.apool);
+      if (op.attribs.indexOf(attributeIndex) !== -1) {
+        region = rep.alltext.slice(
+          currentPosition, currentPosition + op.chars
+        );
+        regions.push(region);
+      }
+      currentPosition = currentPosition + op.chars;
+    }
+  });
+
+  return regions;
+}
+
+function listsWithSameElements(list1, list2) {
+  if (list1.length === 0 && list2.length === 0) {
+    return true;
+  }
+
+  var list2HasList1Elems = list1.reduce(function (currentResult, element) {
+    var found = list2.indexOf(element) !== -1;
+    return currentResult && found;
+  }, true);
+
+  var list1HasList2Elems = list2.reduce(function (currentResult, element) {
+    var found = list1.indexOf(element) !== -1;
+    return currentResult && found;
+  }, true);
+
+  return list2HasList1Elems && list1HasList2Elems;
+}
 
 exports.aceEditEvent = function (hook, context) {
   //console.log("readonly_experiment: rep is " + JSON.stringify(context.rep));
@@ -33,6 +89,14 @@ exports.aceEditEvent = function (hook, context) {
   // 3. wait for the event with a backset
   // 4. call ace2_inner.doUndoRedo
 
+  // The above doesn't work when the caret is just beside
+  // a read-only region.
+
+  // It should be better to find all read-only regions before
+  // and after a change and revert the change if the regions after
+  // are different from the regions before.
+
+  /*
   var myAttrMan;
   var wasInsideReadOnlyRegion = false;
 
@@ -47,4 +111,89 @@ exports.aceEditEvent = function (hook, context) {
       }
     }
   }
+  */
+
+  /*
+aline *0+k|1+1 op {"opcode":"+","chars":20,"lines":0,"attribs":"*0"} value true
+aline *0+k|1+1 op {"opcode":"+","chars":1,"lines":1,"attribs":""} value
+aline *1*2*3+4|1+1 op {"opcode":"+","chars":4,"lines":0,"attribs":"*1*2*3"} value
+aline *1*2*3+4|1+1 op {"opcode":"+","chars":1,"lines":1,"attribs":""} value
+
+curl "localhost:9001/api/1/setHTML?apikey=babysfirstetherpadapikey&padID=test" --data-urlencode "html=<html><body><span class="rox">Read-Only 1</span> ok to modify here <span class="rox">Read-Only 2 diff len</span></body></html>"
+
+         1         2         3         4         5
+123456789012345678901234567890123456789012345678901
+Read-Only 1 ok to modify here Read-Only 2 diff len
+           ^                  ^                   ^
+
+aline *0+b+j*0+k|1+1 op {"opcode":"+","chars":11,"lines":0,"attribs":"*0"} value true
+
+aline *0+b+j*0+k|1+1 op {"opcode":"+","chars":19,"lines":0,"attribs":""} value
+
+aline *0+b+j*0+k|1+1 op {"opcode":"+","chars":20,"lines":0,"attribs":"*0"} value true
+
+aline *0+b+j*0+k|1+1 op {"opcode":"+","chars":1,"lines":1,"attribs":""} value
+
+  */
+
+  // Second attempt:
+  // 1. Get the number for the read-only attribute from the attribute pool
+  // 2. get the read-only regions
+  // 3. compare with the regions found previously
+  //
+  // The test I have with a line that contains a read-only region
+  // and a writable region (region without the read-only class) fails.
+  // The editor did not let me write on the writable region.
+  // The read-only attribute association in rep.alines disappears.
+  // So this code thinks the read-only region was deleted
+  // and calls the undo.
+  //
+  // Although etherpad clobbers the read-only attribute association,
+  // it is possible to have regions with different attributes
+  // in the same line.
+  // I edited a pad from two browsers and saw alines with
+  // two regions with different attributes in a single line.
+  var regions = getReadOnlyRegions(context.rep);
+
+  if (readOnlyRegions === null || readOnlyRegions.length === 0) {
+    console.log("assign readOnlyRegions: " + JSON.stringify(regions));
+    readOnlyRegions = regions;
+  }
+
+  // this would have worked if etherpad maintained
+  // the read-only attribute associations in alines
+  /*
+  var equalRegions;
+  var emptyCurrentRegions = false;
+  if (context.callstack.docTextChanged) {
+    console.log("docTextChanged");
+    regions = getReadOnlyRegions(context.rep);
+    emptyCurrentRegions = regions.length === 0;
+    console.log("regions: " + JSON.stringify(regions));
+    equalRegions = listsWithSameElements(readOnlyRegions, regions)
+    console.log("equalRegions " + equalRegions);
+    if (!emptyCurrentRegions && !equalRegions) {
+      console.log("undo");
+      context.editorInfo.ace_doUndoRedo("undo");
+    }
+  }
+  */
+
+  var equalRegions;
+  if (context.callstack.docTextChanged) {
+    console.log("docTextChanged");
+    regions = getReadOnlyRegions(context.rep);
+    console.log("regions: " + JSON.stringify(regions));
+    equalRegions = listsWithSameElements(readOnlyRegions, regions)
+    console.log("equalRegions " + equalRegions);
+    if (!equalRegions) {
+      console.log("undo");
+      context.editorInfo.ace_doUndoRedo("undo");
+    }
+  }
+
+/*
+curl "localhost:9001/api/1/setHTML?apikey=babysfirstetherpadapikey&padID=test" --data-urlencode "html=<html><body><span class="rox">First Read-Only Line</span><br>ok to modify here<br><span class="rox">Second Read-Only line</span><br>can write anywhere from this point on</body></html>"
+*/
+
 };
